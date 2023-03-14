4b7d2204812789ec3c1748ccb34f44fb50fbb690
alloyking1 <alloyking1@gmail.com>
2023-03-14 12:20:24 +0100

    configured project and created routes

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,4 @@
+
+
+<h1>index methog of blogs</h1>
+<a href={{ route('blog.show', ['id' => 12]) }}>index</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\FallbackController;
+use App\Http\Controllers\PostsController;
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+Route::prefix('/blog')->group(function () {
+    Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+    Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
+
+    // post
+    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+    Route::post('/blog', [PostsController::class, 'store'])->name('blog.sotre');
+
+    // update
+    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+    Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
+    Route::delete('/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
 });
+
+Route::fallback(FallbackController::class);
+// Route::resource('/blog', PostsController::class);
