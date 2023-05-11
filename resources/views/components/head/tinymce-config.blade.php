834c6d8095587b6f905709ed2572f0835765a1db
alloyking1 <alloyking1@gmail.com>
2023-05-12 00:14:06 +0100

    tinymce cache file cleared and image upload buttno clicked

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -1,69 +1,11 @@
 <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
 <script>
-  tinymce.init({
-    selector: 'textarea#myeditorinstance2', // Replace this CSS selector to match the placeholder element for TinyMCE
-    plugins: 'code table lists codesample wordcount',
-    toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table | codesample',
-    codesample_global_prismjs: true,
-  });
-
-
-
-
-
-
-// tinymce.init({
-//   selector: 'textarea#myeditorinstance',
-//   image_class_list: [
-//     {title: 'img-responsive', value: 'img-responsive'},
-//   ],
-//   height: 500,
-//   setup: function (editor) {
-//       editor.on('init change', function () {
-//           editor.save();
-//       });
-//   },
-//   plugins: 'image code table lists codesample wordcount',
-//   toolbar: 'undo redo | link image | code | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table | codesample',
-//   image_title: true,
-//   automatic_uploads: true,
-//   images_upload_url:'/post-image'
-//   file_picker_types: 'image',
-//   file_picker_callback: function (cb, value, meta) {
-//     var input = document.createElement('input');
-//     input.setAttribute('type', 'file');
-//     input.setAttribute('accept', 'image/*');
-//     input.onchange = function () {
-//       var file = this.files[0];
-
-//       var reader = new FileReader();
-//       reader.onload = function () {
-//         var id = 'blobid' + (new Date()).getTime();
-//         var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-//         var base64 = reader.result.split(',')[1];
-//         var blobInfo = blobCache.create(id, file, base64);
-//         blobCache.add(blobInfo);
-
-//         cb(blobInfo.blobUri(), { title: file.name });
-//       };
-//       reader.readAsDataURL(file);
-//     };
-
-//     input.click();
-//   },
-//   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-// });
-
-</script>
-
-{{-- <script>
 const useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 const isSmallScreen = window.matchMedia('(max-width: 1023.5px)').matches;
 
 tinymce.init({
-  // selector: 'textarea#myeditorinstance',
-  // selector: 'textarea#myeditorinstance',
+  selector: 'textarea#myeditorinstance',
   plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
   editimage_cors_hosts: ['picsum.photos'],
   menubar: 'file edit view insert format tools table help',
@@ -123,4 +65,4 @@ tinymce.init({
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });
 
-</script> --}}
+</script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <x-head.tinymce-config/>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -9,7 +11,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <x-head.tinymce-config/>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
        
     </head>
