3e438e9f313874efd04acb38294646a265f95184
alloyking1 <alloyking1@gmail.com>
2023-05-18 12:30:12 +0100

    added email list and socila media links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,6 @@
                 {{ $slot }}
             </main>
         </div>
-        {{-- <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script> --}}
+        <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     </body>
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -27,7 +27,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        {{-- <x-head.tinymce-config/> --}}
+        <x-head.tinymce-config/>
         <link rel="stylesheet" href="{{ asset('/prism.css') }}">
     </head>
     <body class="font-sans text-gray-900bg-gray-100 antialiased">
