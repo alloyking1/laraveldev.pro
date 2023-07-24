fba0a72a7ef9d343f19d8f801bcf9669052ed968
alloyking1 <alloyking1@gmail.com>
2023-07-24 11:48:06 +0100

    excerpt form loads with data

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -17,117 +17,115 @@
                 @endif
             </div>
 
-            @if($post)
             @foreach ($post as $postValue )
+                <x-blog.components.card title="Edit Post" subtitle="Use the form below to edit post..">
+                    <form
+                        action="{{ route('blog.save') }}"
+                        method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
 
-
-            <x-blog.components.card title="Edit Post" subtitle="Use the form below to edit post..">
-                <form
-                    action="{{ route('blog.save') }}"
-                    method="POST"
-                    enctype="multipart/form-data">
-                    @csrf
-
-                    <div class="mt-4">
-                        <x-text-input id="title" placeholder="title" class="block mt-1 w-full" type="text" name="title" :value="old('title', $postValue->title ?? '' )" required autofocus autocomplete="title" />
-                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
-                    </div>
-                    <div class="mt-4">
-                        <textarea placeholder="Enter Excerpt" :value="old('excerpt', $postValue->excerpt ?? '')" name="excerpt" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" rows="6" cols="80" type="text"></textarea>
-                        <x-input-error :messages="$errors->get('excerpt')" class="mt-2" />
-                    </div>
-                    <div class="mt-4 grid grid-cols-3 gap-2">
-                        <div>
-                            <x-text-input id="min_to_read" placeholder="Minutes to read" class="block mt-1 w-full" type="number" name="min_to_read" :value="old('min_to_read', $postValue->min_to_read ?? '')" required autofocus autocomplete="min_to_read" />
-                            <x-input-error :messages="$errors->get('min_to_read')" class="mt-2" />
+                        <div class="mt-4">
+                            <x-text-input id="title" placeholder="title" class="block mt-1 w-full" type="text" name="title" :value="old('title', $postValue->title ?? '' )" required autofocus autocomplete="title" />
+                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
                         </div>
-                        <div class="mt-1">
-                            <x-input-error :messages="$errors->get('category')" class="" />
-                            
-                            <select name="category" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" id="">
-                                @foreach ($postValue->category as $selectedCategory )
-                                    <option value="#" disabled>Select Category</option>
-                                    @foreach ($category as $otherCategories)
-                                    @if ($selectedCategory->name == $otherCategories->name)
-                                        <option value="{{ $otherCategories->id }}" selected> {{ $otherCategories->title }}</option>
-                                    @else
-                                        <option value="{{ $otherCategories->id}}"> {{ $otherCategories->title }}</option>
-                                    @endif
-                                    @endforeach
-                                @endforeach
-                            </select>
-                           
+                        <div class="mt-4">
+                            <textarea placeholder="Enter Excerpt" :value="old('excerpt', $postValue->excerpt)" name="excerpt" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" rows="6" cols="80" type="text">
+                            {{ $postValue->excerpt }}
+                            </textarea>
+                            <x-input-error :messages="$errors->get('excerpt')" class="mt-2" />
                         </div>
-
-                        <div class="mt-1">
-                            <x-input-error :messages="$errors->get('tag')" class="" />
-                                <select name="tag" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" id="">
-                                    <option value="#" disabled>Select Tag</option>
-                                    @foreach ($postValue->tag as $selectedTag )
-                                        @foreach ($tag as $otherTags)
-                                            @if ($selectedTag->name == $otherTags->name)
-                                                <option value="{{ $otherTags->id }}" selected> {{ $otherTags->name }}</option>
-                                            @else
-                                                <option value="{{ $otherTags->id }}"> {{ $otherTags->name }}</option>
-                                            @endif
+                        <div class="mt-4 grid grid-cols-3 gap-2">
+                            <div>
+                                <x-text-input id="min_to_read" placeholder="Minutes to read" class="block mt-1 w-full" type="number" name="min_to_read" :value="old('min_to_read', $postValue->min_to_read ?? '')" required autofocus autocomplete="min_to_read" />
+                                <x-input-error :messages="$errors->get('min_to_read')" class="mt-2" />
+                            </div>
+                            <div class="mt-1">
+                                <x-input-error :messages="$errors->get('category')" class="" />
+                                
+                                <select name="category" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" id="">
+                                    @foreach ($postValue->category as $selectedCategory )
+                                        <option value="#" disabled>Select Category</option>
+                                        @foreach ($category as $otherCategories)
+                                        @if ($selectedCategory->name == $otherCategories->name)
+                                            <option value="{{ $otherCategories->id }}" selected> {{ $otherCategories->title }}</option>
+                                        @else
+                                            <option value="{{ $otherCategories->id}}"> {{ $otherCategories->title }}</option>
+                                        @endif
                                         @endforeach
                                     @endforeach
                                 </select>
+                            
+                            </div>
+
+                            <div class="mt-1">
+                                <x-input-error :messages="$errors->get('tag')" class="" />
+                                    <select name="tag" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" id="">
+                                        <option value="#" disabled>Select Tag</option>
+                                        @foreach ($postValue->tag as $selectedTag )
+                                            @foreach ($tag as $otherTags)
+                                                @if ($selectedTag->name == $otherTags->name)
+                                                    <option value="{{ $otherTags->id }}" selected> {{ $otherTags->name }}</option>
+                                                @else
+                                                    <option value="{{ $otherTags->id }}"> {{ $otherTags->name }}</option>
+                                                @endif
+                                            @endforeach
+                                        @endforeach
+                                    </select>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="mt-4">
-                    <x-forms.tinymce-editor name="body" value="{{ $postValue->body }}"/>
-                    </div>
-
-                    <div class="mt-4">
-                        <x-input-label for="is_published" :value="__('Published')" />
-                        <x-text-input id="is_published" placeholder="Minutes to read" class="block mt-1" type="checkbox" name="is_published" :value="old('is_published')" autofocus autocomplete="min_to_read" />
-                        <x-input-error :messages="$errors->get('is_published')" class="mt-2" />
-                    </div>
-
-                    <div class="mt-4 grid grid-cols-3 gap-2">
                         <div class="mt-4">
-                            <x-text-input id="meta_description" placeholder="Meta Description.." class="block mt-1 w-full" type="text" name="meta_description" :value="old('meta_description', $postValue->meta_description ?? '')" required autofocus autocomplete="meta_description" />
-                            <x-input-error :messages="$errors->get('meta_description')" class="mt-2" />
+                        <x-forms.tinymce-editor name="body" value="{{ $postValue->body }}"/>
                         </div>
+
                         <div class="mt-4">
-                            <x-text-input id="meta_keywords" placeholder="Meta Keywords.." class="block mt-1 w-full" type="text" name="meta_keywords" :value="old('meta_keywords', $postValue->meta_keywords ?? '')" required autofocus autocomplete="meta_keywords" />
-                            <x-input-error :messages="$errors->get('meta_keywords')" class="mt-2" />
+                            <x-input-label for="is_published" :value="__('Published')" />
+                            <x-text-input id="is_published" placeholder="Minutes to read" class="block mt-1" type="checkbox" name="is_published" :value="old('is_published')" autofocus autocomplete="min_to_read" />
+                            <x-input-error :messages="$errors->get('is_published')" class="mt-2" />
                         </div>
+
+                        <div class="mt-4 grid grid-cols-3 gap-2">
+                            <div class="mt-4">
+                                <x-text-input id="meta_description" placeholder="Meta Description.." class="block mt-1 w-full" type="text" name="meta_description" :value="old('meta_description', $postValue->meta_description ?? '')" required autofocus autocomplete="meta_description" />
+                                <x-input-error :messages="$errors->get('meta_description')" class="mt-2" />
+                            </div>
+                            <div class="mt-4">
+                                <x-text-input id="meta_keywords" placeholder="Meta Keywords.." class="block mt-1 w-full" type="text" name="meta_keywords" :value="old('meta_keywords', $postValue->meta_keywords ?? '')" required autofocus autocomplete="meta_keywords" />
+                                <x-input-error :messages="$errors->get('meta_keywords')" class="mt-2" />
+                            </div>
+                            <div class="mt-4">
+                                <x-text-input id="meta_robots" placeholder="Meta Robots..." class="block mt-1 w-full" type="text" name="meta_robots" :value="old('meta_robots', $postValue->meta_robots ?? '')" required autofocus autocomplete="meta_robots" />
+                                <x-input-error :messages="$errors->get('meta_robots')" class="mt-2" />
+                            </div>
+                        </div>
+
                         <div class="mt-4">
-                            <x-text-input id="meta_robots" placeholder="Meta Robots..." class="block mt-1 w-full" type="text" name="meta_robots" :value="old('meta_robots', $postValue->meta_robots ?? '')" required autofocus autocomplete="meta_robots" />
-                            <x-input-error :messages="$errors->get('meta_robots')" class="mt-2" />
+                                <x-input-error :messages="$errors->get('grade')" class="" />
+                                    <select name="grade" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" id="">
+                                        <option value="#" disabled>Select Tag</option>
+                                        @foreach (App\Enums\BLogPostGradeEnum::cases() as $item)
+                                            <option value="{{ $postValue->grade->name ?? $item }}"> {{ $item ?? $postValue->grade->name }}</option>
+                                        @endforeach
+                                    </select>
+                            </div>
+
+
+                        <div class="mt-4 flex justify-between">
+                        <button
+                            type="submit"
+                            class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-2xl">
+                            Save Post
+                        </button>
+                        <button
+                            type="submit"
+                            class="uppercase mt-15 bg-green-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-2xl">
+                            Publish post
+                        </button>
                         </div>
-                    </div>
-
-                    <div class="mt-4">
-                            <x-input-error :messages="$errors->get('grade')" class="" />
-                                <select name="grade" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full" id="">
-                                    <option value="#" disabled>Select Tag</option>
-                                    @foreach (App\Enums\BLogPostGradeEnum::cases() as $item)
-                                        <option value="{{ $postValue->grade->name ?? $item }}"> {{ $item ?? $postValue->grade->name }}</option>
-                                    @endforeach
-                                </select>
-                        </div>
-
-
-                    <div class="mt-4 flex justify-between">
-                    <button
-                        type="submit"
-                        class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-2xl">
-                        Save Post
-                    </button>
-                    <button
-                        type="submit"
-                        class="uppercase mt-15 bg-green-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-2xl">
-                        Publish post
-                    </button>
-                    </div>
-                </form>
-            </x-blog-components.card>
+                    </form>
+                </x-blog-components.card>
             @endforeach
-            @endif
     </div>
 
 </x-app-layout>
