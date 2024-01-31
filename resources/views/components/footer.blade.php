ec8fcce5d1d35714c7f1edc4b62a13e59ec12e99
alloyking1 <alloyking1@gmail.com>
2024-01-31 22:26:17 +0100

    feature:added a mailist form

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,8 @@
 <div>
-    <x-blog.pages.section color="black" title="Footer" textColor="white" textSize="large">
-        <x-blog.pages.grid-3 class="">
+    <x-blog.pages.section color="black" title="" textColor="white" textSize="large">
+        <x-blog.pages.grid-1 class="text-center">
+            <x-blog.text.text color="white" textSize="small" value="Subscribe to Our Mailing List"/>
             <x-blog.components.email-list/>
-        </x-blog.pages.grid-3>
+        </x-blog.pages.grid-1>
     </x-blog.pages.section>
 </div>
