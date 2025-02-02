dde348d579c7c6a46276dd8dda8be10759707d3a
alloyking1 <alloyking1@gmail.com>
2025-02-02 17:30:11 +0100

    feature - landing page refactor

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,46 +1,35 @@
 <x-guest-layout>
     <x-blog.seo.page-meta
-      title="Laraveldev.pro - laravel consultants |laravel agencies | post laravel agency | laravel consultants recruitment"
-      description="A laravel jobboard for Laravel agencies and developers with a deep technical blog for Laravel developers - posting laravel jobs | posting vue.js jobs | posting laravel TALL stack jobs | find laravel jobs | apply to laravel jobs online | get hired as a laravel developer"
-      keywords="laravel,job,recruitment,laravel-hire,job-board,laraveljobs.com, laravelnews.com, laracast, laraveldev.pro, laravel hire"
-      robots="laravel,job,recruitment,laravel-hire,job-board,laraveljobs.com, laravelnews.com, laracast, laraveldev.pro, laravel hire"
+      title="Laravel Tutorials & Best Practices | Learn PHP & Laravel Development | LaravelDev.pro"
+      description="Learn Laravel development through comprehensive tutorials, books, courses, best practices, and expert tips. Join our community of PHP/Laravel developers to enhance your skills and stay updated with the latest Laravel trends."
+      keywords="laravel tutorials, laravel development, php development, laravel best practices, laravel tips, laravel learning, web development, php framework, laravel documentation, laravel community"
+      robots="laravel,tutorials,books,courses,best practices,expert tips,php,laravel development,web development,php framework,laravel documentation,laravel community"
     />
 
     <x-blog.pages.section color="black" textColor="white" textSize="large" class="relative">
-        <div class="">
+        <div class="h-screen mt-16">
           <div class="text-center grid place-content-center justify-center">
-            <x-blog.text.text textSize="header2" color="white" value="Looking to hire a Laravel dev?" class="font-black"/>
+            <x-blog.text.text textSize="header2" color="white" value="Learn Laravel | Tutorials, Tips & Best Practices" class="font-black"/>
     
             <div class="max-w-2xl mx-auto mt-4">
               <x-blog.text.text textSize="x-small"
               color="white"
               class="font-thin"
-              value="Post your job and receive an impressive number of high-quality applications from top developers. Start finding the perfect fit for your project today!"/>
+              value="We are dedicated to making you a better Laravel developer through a wide range of resources to help you learn and grow as a PHP/Laravel developer."/>
             
-              <x-blog.pages.grid-1 class="mt-4">
-                @forelse ($jobs as $job)
-                    <a href="{{ $job->application_link }}" target="_blank" class="text-black">
-                        <x-blog.components.job-card :job="$job"/>
-                    </a>
-                @empty
-                    <x-blog.components.card-long>
-                        <x-blog.text.text textSize="xx-small" color="gray" value="Silence is golden!" class=""/>
-                    </x-blog.components.card-long>
-                @endforelse
+                <x-blog.pages.grid-1 class="mt-16">
+                    <x-blog.text.text color="white" class="" textSize="medium" value="Join our community of Laravel professionals for expert insights and advanced tutorials"/>
+                    <div class="">
+                        <x-blog.components.email-list/>
+                    </div>
                 </x-blog.pages.grid-1>
-
-                <div class="flex justify-end">
-                    <a href="{{ route('jobs.all') }}" class="border rounded-sm p-1 text-xs mt-2">
-                        <div class="text-white">All jobs >></div>
-                    </a>
-                </div>
             </div>
           </div>
         </div>
     </x-blog.pages.section>
 
 
-    <x-blog.pages.section color="black" textColor="white" textSize="large" class="relative">
+    {{-- <x-blog.pages.section color="black" textColor="white" textSize="large" class="relative">
         <div class="shadow-lg border border-gray-50 p-16 pb-24 rounded-2xl">
             <x-blog.pages.grid-2 class="pt-8">
 
@@ -74,5 +63,5 @@
                 </a>
             </div>
         </div>
-    </x-blog.pages.section>
+    </x-blog.pages.section> --}}
 </x-guest-layout>
